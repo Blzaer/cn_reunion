2d0968be250cb7fd60f7676ce0ef3047a7a5a5bc
wangchao_8345@qq.com <>
2022-11-28 16:09:26 +0800

    fix: 持续构建修改

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:Blzaer/cn_reunion.git master
+git push -f git@github.com:Blzaer/Blzaer.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
